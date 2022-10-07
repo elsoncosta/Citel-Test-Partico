53848246e53f28cd74d967af9c62500ca5fe095c
Elson Costa <ellsoncostas@gmail.com>
2022-10-07 14:40:36 -0300

    add cors

---
+++ src/CitelTestPratico.WebAPI/Configuration/AppCorsConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Data;
+
+namespace CitelTestPratico.WebAPI.Configuration
+{
+    public static class AppCorsConfig
+    {
+        public static void AddDatabaseSetup(this WebApplicationBuilder builder, WebApplication app)
+        {
+            var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(MyAllowSpecificOrigins,
+                                      policy =>
+                                      {
+                                          policy.WithOrigins("http://127.0.0.1.com",
+                                                              "http://localhost:4200")
+                                                              .AllowAnyHeader()
+                                                              .AllowAnyMethod();
+                                      });
+            });
+
+            app.UseCors(MyAllowSpecificOrigins);
+        }
+    }
+}
